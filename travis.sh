a0149cb517810f49e6fa2217a973a52f626bbd55
Jan Doms <jan.doms@gmail.com>
2015-08-05 22:53:35 +0200

    travis lwt.2.5.0

--- travis.sh
+++ travis.sh
@@ -10,9 +10,9 @@ OPAM_DEPENDS="ocamlfind \
          ssl.0.5.0 \
          camlbz2.0.6.0 \
          snappy.0.1.0 \
-         lwt.2.4.8 \
+         lwt.2.5.0 \
          camltc.0.9.2 \
-         cstruct.1.6.0 \
+         cstruct.1.7.0 \
          ctypes.0.4.1 \
          ctypes-foreign.0.4.0 \
          bisect.1.3 \
