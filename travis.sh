203532554698acdf9038a034eb84a713ae514a8f
Jan Doms <jan.doms@gmail.com>
2015-08-13 11:12:12 +0200

    enable recovery test suite

--- travis.sh
+++ travis.sh
@@ -124,6 +124,9 @@ script () {
             fab dev.run_tests_disk_failures
             fab alba.smoke_test
             ;;
+        recovery)
+            fab dev.run_tests_recovery
+            ;;
         *)
             echo "invalid test suite specified..."
             exit1
