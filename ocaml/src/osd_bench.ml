cfb4a27fa2cf1ed6eb9fc73c3581a0344583b1fd
Romain Slootmaekers <romain.slootmaekers@cloudfounders.com>
2015-08-17 14:51:02 +0200

    rough draft of benchmark

--- ocaml/src/osd_bench.ml
+++ ocaml/src/osd_bench.ml
@@ -16,81 +16,12 @@ limitations under the License.
 
 open Asd_protocol
 open Lwt
+open Generic_bench
 
-let progress t0 step i =
-  let row = 10 * step in
-  if i mod step = 0 && i <> 0
-  then
-    Lwt_io.printf "%16i" i >>= fun () ->
-    if (i mod row = 0 )
-    then
-      let t1 = Unix.gettimeofday() in
-      let dt = t1 -. t0 in
-      let speed = (float i) /. dt in
-      Lwt_io.printlf " (%8.2fs;%6.2f/s)" dt speed
-    else Lwt.return ()
-  else Lwt.return ()
-
-
-let measured_loop f n =
-  let t0 = Unix.gettimeofday () in
-  let step = n / 100 in
-  let rec loop min_d max_d i =
-    if i = n
-    then Lwt.return (min_d, max_d)
-    else
-      progress t0 step i >>= fun () ->
-      let t1 = Unix.gettimeofday() in
-      f i >>= fun () ->
-      let t2 = Unix.gettimeofday() in
-      let d = t2 -. t1 in
-      let min_d' = min d min_d
-      and max_d' = max d max_d
-      in
-      loop min_d' max_d' (i+1)
-  in
-  loop max_float 0. 0 >>= fun (min_d, max_d) ->
-  let t1 = Unix.gettimeofday () in
-  let d = t1 -. t0 in
-  let nf = float n in
-  let speed = nf /. d in
-  let latency = d /. nf in
-  Lwt.return (d,speed, latency, min_d, max_d)
-
-let report name (d,speed, latency, min_d, max_d) =
-  Lwt_io.printlf "\n%s" name >>= fun () ->
-  Lwt_io.printlf "\ttook: %fs or (%f /s)" d speed >>= fun () ->
-  Lwt_io.printlf "\tlatency: %fms" (latency *. 1000.0) >>= fun () ->
-  Lwt_io.printlf "\tmin: %fms" (min_d *. 1000.0) >>= fun () ->
-  Lwt_io.printlf "\tmax: %fms" (max_d *. 1000.0)
-
-let _make_key period prefix =
-  (* math:
-     X_{n+1} = (aX_{n} + c) mod m
-     if a = 21, c = 3 and m = 10^ x
-     then this one is of full period
-    *)
-  let x = ref 42 in
-  fun () ->
-    let i = !x in
-    let x' = (21 * !x + 3) mod period in
-    let () = x := x'  in
-    Printf.sprintf
-      "%s%s_%016i"
-      Osd_keys.bench_prefix prefix i
-
-let make_key power prefix =
-  let period =
-    let rec loop acc = function
-      | 0 -> acc
-      | i -> loop (10 * acc) (i-1)
-    in
-    loop 1 power
-  in
-  _make_key period prefix
 
 let deletes (client: Osd.osd) n value_size power prefix =
-  let gen = make_key power prefix in
+  let period = period_of_power power in
+  let gen = make_key period prefix in
   let do_one i =
     let key = gen () in
     let open Slice in
@@ -105,7 +36,8 @@ let deletes (client: Osd.osd) n value_size power prefix =
 
 
 let gets (client: Osd.osd) n value_size power prefix =
-  let gen = make_key power prefix in
+  let period = period_of_power power in
+  let gen = make_key period prefix in
   let do_one i =
     let key = gen () in
     let open Slice in
@@ -122,7 +54,8 @@ let gets (client: Osd.osd) n value_size power prefix =
   report "gets" r
 
 let sets (client:Osd.osd) n value_size power prefix =
-  let gen = make_key power prefix in
+  let period = period_of_power power in
+  let gen = make_key period prefix in
   (* TODO: this affects performance as there is compression going
      on inside the database
    *)
