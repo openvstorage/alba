e9608c27ee62425fed3446bdacd850566393e113
Romain Slootmaekers <romain.slootmaekers@cloudfounders.com>
2015-08-20 14:38:33 +0200

    split fab smoke test from archiving

--- jenkins/050-smoke_test.sh
+++ jenkins/050-smoke_test.sh
@@ -8,9 +8,3 @@ fab alba.smoke_test
 
 pkill alba.native
 pkill arakoon
-
-# archive the artifacts
-mkdir -p ~/archives
-tar -czvf ~/archives/$BUILD_TAG.tar.gz -C /tmp . || true
-
-sudo rm -rf /tmp/*
