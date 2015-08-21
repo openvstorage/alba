1a128c26e1fefafc836ba800ff7dcb3094984b71
Romain Slootmaekers <romain.slootmaekers@cloudfounders.com>
2015-08-21 19:00:40 +0200

    this seems to have been forgotten

--- jenkins/cpp/010-build_client.sh
+++ jenkins/cpp/010-build_client.sh
@@ -9,4 +9,15 @@ cd ./cpp/automake_client/
 make clean
 make
 
+cd ../
+if [ ! -d ".tup" ]; then
+    tup init
+    printf 'CONFIG_COMPILER=clang++-3.5\n' > ./tup.config
+
+fi
+
+rm -f $(find ./ -name "*.o")
+rm -f lib/*
+rm -f bin/*
+
 tup
