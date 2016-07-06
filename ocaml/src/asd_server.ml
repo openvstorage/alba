555d872dcf76355f6367c2f44ed73c6837d5999a
Jan Doms <jan.doms@openvstorage.com>
2016-07-06 15:21:20 +0200

    fix failing test

--- ocaml/src/asd_server.ml
+++ ocaml/src/asd_server.ml
@@ -1604,6 +1604,13 @@ let run_server
       ~get_next_fnr
       asd_id nfd
   in
+  let wrap_t t name =
+    Lwt.catch
+      (fun () -> t)
+      (fun exn ->
+       Lwt_log.info_f ~exn "Exception in thread %s" name >>= fun () ->
+       Lwt.fail exn)
+  in
   let maybe_add_plain_server threads =
     match port with
     | None -> threads
@@ -1614,7 +1621,7 @@ let run_server
                  ~tcp_keepalive
                  ~tls:None protocol
        in
-       t :: threads
+       (wrap_t t "plain server") :: threads
   in
   let maybe_add_tls_server threads =
       match tls with
@@ -1626,7 +1633,7 @@ let run_server
          let t = Networking2.make_server ?cancel hosts tlsPort
                                          ~transport ~tcp_keepalive ~tls protocol
          in
-         t :: threads
+         (wrap_t t "tls server") :: threads
   in
   let maybe_add_multicast threads =
     match multicast with
@@ -1658,7 +1665,7 @@ let run_server
            mcast_period
            ~disk_usage
        in
-       mcast_t () :: threads
+       (wrap_t (mcast_t ()) "multicast") :: threads
   in
   let reporting_t =
     let section = AsdStatistics.section in
@@ -1674,9 +1681,9 @@ let run_server
   in
   let threads =
     [
-      (Lwt_extra2.make_fuse_thread ());
-      reporting_t;
-      io_sched_t;
+      wrap_t (Lwt_extra2.make_fuse_thread ()) "fuse thread";
+      wrap_t reporting_t "reporting_t";
+      wrap_t io_sched_t "io_sched_t";
     ]
     |> maybe_add_plain_server
     |> maybe_add_tls_server
