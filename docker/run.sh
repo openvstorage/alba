f24da7984103a56a7e6e1e0068b8ce0a53d8c7e0
Jan Doms <jan.doms@openvstorage.com>
2016-08-08 15:53:52 +0200

    allow running voldrv testers in the ubuntu-14.04 image

--- docker/run.sh
+++ docker/run.sh
@@ -7,13 +7,7 @@ docker build --rm=true --tag=alba_$IMAGE ./docker/$IMAGE
 
 if [ -t 1 ];
 then TTY="-t";
-else
-    # this path is taken on jenkins, clean previous builds first
-    TTY="";
-    docker run -i $TTY --privileged=true -e UID=${UID} \
-           -v ${PWD}:/home/jenkins/alba \
-           -w /home/jenkins/alba alba_$IMAGE \
-           bash -l -c "cd alba && ./jenkins/run2.sh clean"
+else TTY="";
 fi
 
 docker run -i $TTY --privileged=true -e UID=${UID} \
