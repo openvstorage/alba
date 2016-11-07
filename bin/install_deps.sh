b5f7bac71c818ac38b00e0a52876566abca7960b
Romain Slootmaekers <romain.slootmaekers@openvstorage.com>
2016-11-07 17:31:05 +0100

    update install_deps

--- bin/install_deps.sh
+++ bin/install_deps.sh
@@ -1,9 +1,39 @@
-sudo apt-get install \
-    build-essential \
-    autoconf \
-    libtool \
-    libboost-all-dev \
-    libev-dev \
-    libsnappy-dev \
-    libbz2-dev \
-    python-virtualenv \
+apt-get install -y \
+        build-essential m4 apt-utils \
+        libffi-dev libssl-dev \
+        libbz2-dev \
+        libgmp3-dev \
+        libev-dev \
+        libsnappy-dev \
+        libxen-dev \
+        help2man \
+        pkg-config \
+        time \
+        aspcud \
+        wget \
+        rsync \
+        darcs \
+        git \
+        unzip \
+        protobuf-compiler \
+        libgcrypt20-dev \
+        libjerasure-dev \
+        yasm \
+        automake \
+        python-dev \
+        python-pip \
+        debhelper \
+        psmisc \
+        strace \
+        curl \
+        g++ \
+        libgflags-dev \
+        sudo \
+        libtool \
+        libboost-all-dev \
+        fuse \
+        sysstat \
+        ncurses-dev \
+        liburiparser1 \
+        libzmq5 \
+        librabbitmq4
