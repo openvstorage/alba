2e2109630d99cce337dff0af7a1f9b5f11d33300
Romain Slootmaekers <romain.slootmaekers@openvstorage.com>
2017-05-30 11:56:08 +0200

    move validation to construction iso usage

--- ocaml/src/tools/networking2.ml
+++ ocaml/src/tools/networking2.ml
@@ -131,7 +131,13 @@ type conn_info = {
     tls_config: Tls.t option
   } [@@deriving show]
 
-let make_conn_info ips port ?(transport=Net_fd.TCP) tls_config = {ips;port;transport;tls_config}
+let make_conn_info ips port ?(transport=Net_fd.TCP) tls_config =
+  let () =
+    match ips with
+    | [] -> failwith "empty ips list for asd_client.with_client";
+    | _ -> ()
+  in
+  {ips;port;transport;tls_config}
 
 exception No_connection
 
